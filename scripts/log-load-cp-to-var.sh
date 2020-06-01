5a12fbc04685fadcdce68848245a01a39c562885
martinrsdo <martin.rsdo@gmail.com>
2020-06-01 19:56:32 -0300

    Add some comments

--- scripts/log-load-cp-to-var.sh
+++ scripts/log-load-cp-to-var.sh
@@ -1,8 +1,14 @@
 #/bin/sh
 
+#define some variables
 CPDATE=$(date "+%Y%m%dT%H%M%S")
+
+#Non volatile path for permanent storage of the data
 DIR="/var/log/activityload"
 
+#create directory if not exist
 mkdir -p $DIR
+
+#Copy and then remove temp data
 cp /tmp/log-activity/temp.log $DIR/load_$CPDATE.log && rm /tmp/log-activity/temp.log
 
--- scripts/log-load-to-tmp.sh
+++ scripts/log-load-to-tmp.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-
-
-#Creo el directorio si no existe
+#Create directory if not exists
 mkdir -p /tmp/log-activity
 
-#Add a new line to the temporary log file 
-#Timestamp, Average Load, Available Memory follow by the Top10 Process order by CPU usage
+#Add a new line to the temporary log file
+#Timestamp using date command
+#Average Load provided in /proc/loadavg
+#Available Memory provided by free command
+#Top10 Process order by CPU usage provided by ps command
+
 echo $(date "+%Y%m%dT%H%M%S")","$(cat /proc/loadavg | cut -d' ' -f1)","$(echo $(free -k) | cut -d' ' -f13)","$(ps axo comm --no-headers --sort=-pcpu | head -n 10 |tr '\n' ',') >> /tmp/log-activity/temp.log
 
 
